7210b5e334b305bdae5e2f4349421241bdb611ee
Adnan <adnan.muhammad@croydon.gov.uk>
2020-09-17 11:47:56 +0100

    Directory search block.
    Coding standards fix for constant declaration.
    
    Issue #30

---
+++ src/Constants.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\localgov_directories;
+
+/**
+ * Constants for this module.
+ */
+class Constants {
+
+  const CHANNEL_SELECTION_FIELD = 'localgov_directory_channels';
+
+  const CHANNEL_SEARCH_BLOCK = 'localgov_directories_channel_search_block';
+
+}
