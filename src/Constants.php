405badaf15379cd13bcb225df203b024f35fda6e
Adnan <adnan.muhammad@croydon.gov.uk>
2020-09-28 19:41:23 +0100

    Teaser view: Defer indexing of the Facet selection field.
    Bringing back the Views config update code.  For this to work, we have to defer
    setting up indexing on the Facet selection field.  A Directory entry content
    type may not have the Facet selection field when the Channel selection field is
    added to it.  This breaks the Search index.  Deferring indexing of the Facet
    selection field until this field is actually added to a Directory entry content
    type resolves this.
    
    Issue #38

--- src/Constants.php
+++ src/Constants.php
@@ -13,4 +13,8 @@ class Constants {
 
   const CHANNEL_SEARCH_BLOCK = 'localgov_directories_channel_search_block';
 
+  const FACET_INDEXING_FIELD = 'localgov_directory_facets_filter';
+
+  const FACET_SELECTION_FIELD = 'localgov_directory_facets_select';
+
 }
