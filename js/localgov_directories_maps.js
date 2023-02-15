ced4aa6a1a5e14129211285d4fff7da3f312105e
Mark Conroy <mark@annertech.com>
2023-02-15 18:01:44 +0100

    removes console.log

--- js/localgov_directories_maps.js
+++ js/localgov_directories_maps.js
@@ -5,7 +5,6 @@ Drupal.behaviors.localgovDirectoriesMaps = {
 
     maps.forEach(map => {
       const mapParentId = map.closest('.field-group-tab').getAttribute('id');
-      console.log(mapParentId);
       const correspondingLink = context.querySelector(`[href="#${mapParentId}"]`);
       correspondingLink.addEventListener('click', () => {
         window.dispatchEvent(new Event('resize'));
