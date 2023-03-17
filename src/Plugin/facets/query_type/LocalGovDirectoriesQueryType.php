e0ff96e59a1ebd0b18de4c6325e95c2368ac09a3
Andy Broomfield <andy.broomfield@brighton-hove.gov.uk>
2023-03-17 10:38:31 +0000

    Add function to fetch sibling facets that would be avalible
    This is to address the issue in this comment
      https://github.com/localgovdrupal/localgov_directories/pull/235#issuecomment-1277842772
    
    Instead of just using the facets from the result set, re-run the query, each
    time eliminating all the facets except each group, and filter out its own
    so there is a list of each sibling facet that could be set with an OR that
    also mathces an AND in the other group.
    This then produces a filtered list of sibling facets that should be reachable
    within the AND accross the groups and OR within them.

--- src/Plugin/facets/query_type/LocalGovDirectoriesQueryType.php
+++ src/Plugin/facets/query_type/LocalGovDirectoriesQueryType.php
@@ -66,32 +66,43 @@ class LocalGovDirectoriesQueryType extends QueryTypePluginBase {
   public function build() {
     $results = $this->results;
     $query_operator = $this->facet->getQueryOperator();
+    $field_identifier = $this->facet->getFieldIdentifier();
+
+    // Get passed in facets.
     $temp_query = $this->query->getOriginalQuery();
     $temp_query->preExecute();
     $conditions = &$temp_query->getConditionGroup()->getConditions();
+
+    // Find all the facet condition groups.
+    $facet_conditions = [];
     foreach ($conditions as $key => $condition) {
-      if ($condition instanceof \Drupal\search_api\Query\ConditionGroupInterface) {
+      if ($condition instanceof ConditionGroupInterface) {
         $tags = $condition->getTags();
         foreach($tags as $tag) {
-          if (strpos($tag, 'facet:localgov_directory_facets_filter.') === 0) {
-            // unset($conditions[$key]);
-            $results = array_merge($results, $this->getGroupFacets($tag));
+          if (strpos($tag, 'facet:' . $field_identifier . '.') === 0) {
+            $facet_conditions[$key] = $tag;
           }
         }
       }
     }
-    $filtered_results[] = reset($results);
-    foreach ($results as $result) {
-      $current_results = array_column($filtered_results, 'filter');
-      if (!in_array($result['filter'], $current_results)) {
-        $filtered_results[] = $result;
-      }
-    }
-    $results = $filtered_results;
-    // $temp_query->execute();
-    // $avalible_facets = $temp_query->getResults()->getExtraData('search_api_facets');
-    // $results = $avalible_facets['localgov_directory_facets_filter'] ?? $this->results;
 
+    // Run query elimnating each facet group and return the resulting facets.
+    $results = $this->results;
+    foreach($facet_conditions as $key => $filter_tag) {
+      $group_facets = $this->getResultingFacetsFromFacetGroupExceptOwn($filter_tag);
+      
+      // Remove any duplicate facets, or they show up multiple times.
+      $group_facets_filtered = array_filter($group_facets, function($item) use ($results) {
+        $facet_ids = array_column($results, 'filter');
+        if (in_array($item['filter'], $facet_ids)) {
+          return FALSE;
+        }
+        return TRUE;
+      });
+      $results = array_merge($results, $group_facets_filtered);
+    }
+
+    // Build facets.
     if (!empty($results)) {
       $facet_results = [];
       foreach ($results as $result) {
@@ -114,18 +125,78 @@ class LocalGovDirectoriesQueryType extends QueryTypePluginBase {
     return $this->facet;
   }
 
-  protected function getGroupFacets($filter_tag) {
-    $filter_query = $this->query->getOriginalQuery();
+  /**
+   * Get facets from a facet group except it's own with a corresponding tag
+   * 
+   * Re-run the search api query, this time removing all the other facet groups
+   * except the one specified by the filter tag. The purpose of this is to get
+   * the other facets that would be limited by setting of the passed in groups
+   * facets. It allows us to see the other possible facets that could be 
+   * selected as part of an 'OR' group 'AND' the facets selected in the passed
+   * in group.
+   *
+   * @param String $filter_tag
+   *   The search api tag of the facet group.
+   * @return Array
+   *   Search api query facet results.
+   */
+  protected function getResultingFacetsFromFacetGroupExceptOwn(String $filter_tag):Array {
+
+    // Set up a special filter query which needs to clone the original.
+    $filter_query = clone $this->query->getOriginalQuery();
     $filter_query->preExecute();
+
+    // Find conditions
     $conditions = &$filter_query->getConditionGroup()->getConditions();
+
+    // Store removed conditions so we can reset them.
+    $removed_conditions = [];
+
+    // Loop through each conditions, removing ones that are not this filter tag.
     foreach ($conditions as $tag => $condition) {
-      if ($condition instanceof ConditionGroupInterface && $condition->hasTag($filter_tag)) {
-        unset($conditions[$tag]);
+      if ($condition instanceof ConditionGroupInterface) {
+        $tags = $condition->getTags();
+
+        // @todo Check that we are only removing facet conditions.
+        if (!in_array($filter_tag, $tags)) {
+
+          // Store the removed conditions and remove it.
+          $removed_conditions[$tag] = $conditions[$tag];
+          unset($conditions[$tag]);
+        }
       }
     }
+
+    // Execute the filter query and get the facets returned.
     $filter_query->execute();
     $facets = $filter_query->getResults()->getExtraData('search_api_facets');
-    return $facets['localgov_directory_facets_filter'] ?? [];
+    $filter_query->postExecute();
+
+    // Beacuse for reasons unknown, removing the conditions removes it from all
+    // the following queries, even though we are fetching the original query.
+    // We can get around that by readding the conditions back in now that we 
+    // have the facets we want.
+    $conditions = array_merge($conditions, $removed_conditions);
+
+    // Since we will get every facet from the passed in facet group,
+    // we need to filter those out so checks with other facet groups will
+    // show us only the ones that are reachable from this facet group.
+    $facet_type_id = substr($filter_tag, 39);
+    $group_facet_ids = \Drupal::entityTypeManager()
+      ->getStorage('localgov_directories_facets')
+      ->getQuery()
+      ->condition('bundle', $facet_type_id)
+      ->execute();
+    $found_facets = $facets['localgov_directory_facets_filter'] ?? [];
+    $found_facets = array_filter($found_facets, function($item) use($group_facet_ids) {
+      if (!in_array(intval(trim($item['filter'], '"')), $group_facet_ids)) {
+        return TRUE;
+      }
+      return FALSE;
+    });
+
+    // Finally return the found facets.
+    return $found_facets;
   }
 
 }
