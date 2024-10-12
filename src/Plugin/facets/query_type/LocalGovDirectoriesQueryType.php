0ebecfbda70ed0ca3ac51d825082c29c8372825c
Lee Mills <lee@leemills.dev>
2024-10-12 14:56:53 +0100

    fix: strict types errors

--- src/Plugin/facets/query_type/LocalGovDirectoriesQueryType.php
+++ src/Plugin/facets/query_type/LocalGovDirectoriesQueryType.php
@@ -25,41 +25,39 @@ class LocalGovDirectoriesQueryType extends QueryTypePluginBase {
 
     // Only alter the query when there's an actual query object to alter.
     // @phpstan-ignore-next-line.
-    if (empty($query)) {
-      return;
-    }
+    if (!empty($query)) {
+      $operator = $this->facet->getQueryOperator();
+      $field_identifier = $this->facet->getFieldIdentifier();
+      $exclude = $this->facet->getExclude();
 
-    $operator = $this->facet->getQueryOperator();
-    $field_identifier = $this->facet->getFieldIdentifier();
-    $exclude = $this->facet->getExclude();
-
-    if ($query->getProcessingLevel() === QueryInterface::PROCESSING_FULL) {
-      // Set the options for the actual query.
-      $options = &$query->getOptions();
-      $options['search_api_facets'][$field_identifier] = $this->getFacetOptions();
-    }
-
-    // Add the filter to the query if there are active values.
-    $active_items = $this->facet->getActiveItems();
-
-    if (count($active_items)) {
-      $bundle = NULL;
-      $type_storage = \Drupal::entityTypeManager()
-        ->getStorage('localgov_directories_facets');
-      $chosen_facets = $type_storage->loadMultiple($active_items);
-      foreach ($chosen_facets as $directory_facet) {
-        $bundle[$directory_facet->bundle()][] = $directory_facet->id();
+      if ($query->getProcessingLevel() === QueryInterface::PROCESSING_FULL) {
+        // Set the options for the actual query.
+        $options = &$query->getOptions();
+        $options['search_api_facets'][$field_identifier] = $this->getFacetOptions();
       }
 
-      if ($bundle !== NULL) {
-        $filter = NULL;
-        foreach ($bundle as $bundle_name => $group_items) {
-          unset($filter);
-          $filter = $query->createConditionGroup($operator, ['facet:' . $field_identifier . '.' . $bundle_name]);
-          foreach ($group_items as $value) {
-            $filter->addCondition($this->facet->getFieldIdentifier(), $value, $exclude ? '<>' : '=');
+      // Add the filter to the query if there are active values.
+      $active_items = $this->facet->getActiveItems();
+
+      if (count($active_items)) {
+        $bundle = NULL;
+        $type_storage = \Drupal::entityTypeManager()
+          ->getStorage('localgov_directories_facets');
+        $chosen_facets = $type_storage->loadMultiple($active_items);
+        foreach ($chosen_facets as $directory_facet) {
+          $bundle[$directory_facet->bundle()][] = $directory_facet->id();
+        }
+
+        if ($bundle !== NULL) {
+          $filter = NULL;
+          foreach ($bundle as $bundle_name => $group_items) {
+            unset($filter);
+            $filter = $query->createConditionGroup($operator, ['facet:' . $field_identifier . '.' . $bundle_name]);
+            foreach ($group_items as $value) {
+              $filter->addCondition($this->facet->getFieldIdentifier(), $value, $exclude ? '<>' : '=');
+            }
+            $query->addConditionGroup($filter);
           }
-          $query->addConditionGroup($filter);
         }
       }
     }
@@ -192,7 +190,7 @@ class LocalGovDirectoriesQueryType extends QueryTypePluginBase {
       ->execute();
     $found_facets = $facets['localgov_directory_facets_filter'] ?? [];
     $found_facets = array_filter($found_facets, function ($item) use ($group_facet_ids) {
-      if (in_array(intval(trim($item['filter'], '"')), $group_facet_ids, TRUE)) {
+      if (in_array(intval(trim($item['filter'], '"')), array_map('intval', $group_facet_ids), TRUE)) {
         return TRUE;
       }
       return FALSE;
