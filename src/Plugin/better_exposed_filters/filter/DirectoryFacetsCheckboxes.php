722968bc3246608907e13f4c87d5a394822f1281
Andy Broomfield <talk@andybroomfield.com>
2025-11-24 16:33:12 +0000

    Use instance to add the class resolver service
    This is to maintain compatability with better exposed filters 7.1

--- src/Plugin/better_exposed_filters/filter/DirectoryFacetsCheckboxes.php
+++ src/Plugin/better_exposed_filters/filter/DirectoryFacetsCheckboxes.php
@@ -27,32 +27,12 @@ class DirectoryFacetsCheckboxes extends RadioButtons implements ContainerFactory
   protected $classResolver;
 
   /**
-   * Constructs a DirectoryFacetsCheckboxes object.
-   *
-   * @param array $configuration
-   *   A configuration array containing information about the plugin instance.
-   * @param string $plugin_id
-   *   The plugin ID for the plugin instance.
-   * @param mixed $plugin_definition
-   *   The plugin implementation definition.
-   * @param Drupal\Core\DependencyInjection\ClassResolverInterface $class_resolver
-   *   Class resolver service.
-   */
-  public function __construct(array $configuration, string $plugin_id, $plugin_definition, ClassResolverInterface $class_resolver) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-    $this->classResolver = $class_resolver;
-  }
-
-  /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): static {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('class_resolver'),
-    );
+    $instance = parent::create($container, $configuration, $plugin_id, $plugin_definition);
+    $instance->classResolver = $container->get('class_resolver');
+    return $instance;
   }
 
   /**
